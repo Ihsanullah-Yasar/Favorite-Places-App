4f65b8ba700778b684ca27a5e7d32687586e4533
Ihsanullah-Yasar <ihsanullahyasar70@gmail.com>
2026-02-15 23:36:52 +0430

    feat: place form empty component added

---
+++ components/Places/PlaceForm.tsx
@@ -0,0 +1,20 @@
+import React, { JSX } from "react";
+import { Text, View } from "react-native";
+
+const PlaceForm: React.FC = (): JSX.Element => {
+  return (
+    <View>
+      <Text>The Place Form</Text>
+    </View>
+  );
+};
+
+// function PlaceForm() {
+//   return (
+//     <View>
+//       <Text>The Place Form</Text>
+//     </View>
+//   );
+// }
+
+export default PlaceForm;
