870a80af4b32a5813d96ea9f9960c989adfcfee0
Ihsanullah-Yasar <ihsanullahyasar70@gmail.com>
2026-02-28 01:51:03 +0430

    feat: reusable outlined button cmp added

---
+++ components/UI/OutlinedButton.js
@@ -0,0 +1,43 @@
+import { Ionicons } from "@expo/vector-icons";
+import { Pressable, StyleSheet, Text } from "react-native";
+import { Colors } from "../../constants/colors";
+
+function OutlinedButton({ onPress, icon, children }) {
+  return (
+    <Pressable
+      style={({ pressed }) => [styles.button, pressed && styles.pressed]}
+    >
+      <Ionicons
+        style={styles.icon}
+        size={18}
+        name={icon}
+        color={Colors.primary500}
+      />
+      <Text style={styles.text}>{children}</Text>
+    </Pressable>
+  );
+}
+
+export default OutlinedButton;
+
+const styles = StyleSheet.create({
+  button: {
+    paddingHorizontal: 12,
+    paddingVertical: 6,
+    margin: 4,
+    flexDirection: "row",
+    justifyContent: "center",
+    alignContent: "center",
+    borderWidth: 1,
+    borderColor: Colors.primary500,
+  },
+  pressed: {
+    opacity: 0.7,
+  },
+  icon: {
+    marginRight: 4,
+  },
+  text: {
+    color: Colors.primary500,
+  },
+});
