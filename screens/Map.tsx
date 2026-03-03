8fc4e7df1292c8172467bffa3953148854dac4d7
Ihsanullah-Yasar <ihsanullahyasar70@gmail.com>
2026-03-04 00:32:01 +0430

    feat: Map overview screen added

---
+++ screens/Map.tsx
@@ -0,0 +1,85 @@
+import { NavigationProp } from "@react-navigation/native";
+import { NativeStackScreenProps } from "@react-navigation/native-stack";
+import { FC, useCallback, useLayoutEffect, useState } from "react";
+import { Alert, StyleSheet } from "react-native";
+import MapView, { MapPressEvent, Marker, Region } from "react-native-maps";
+import { RootStackParamList } from "../types/navigation";
+import IconButton from "../components/UI/IconButton";
+
+interface coordinate {
+  lat: number;
+  lng: number;
+}
+
+const INITIAL_REGION: Region = {
+  latitude: 37.78,
+  longitude: -122.43,
+  latitudeDelta: 0.0922,
+  longitudeDelta: 0.0421,
+};
+
+type MapProps = NativeStackScreenProps<RootStackParamList, "Map">;
+
+const Map: FC<MapProps> = ({ navigation }) => {
+  const [selectedLocation, setSelectedLocation] = useState<coordinate | null>(
+    null,
+  );
+
+  const selectLocationHandler = useCallback((event: MapPressEvent) => {
+    const { latitude, longitude } = event.nativeEvent.coordinate;
+    setSelectedLocation({ lat: latitude, lng: longitude });
+  }, []);
+
+  const savePickedLocationHandler = useCallback(() => {
+    if (!selectedLocation) {
+      Alert.alert(
+        "No location picked!",
+        "You have to pick a location (by tapping on the map) first!",
+      );
+      return;
+    }
+    navigation.navigate("AddPlace", {
+      pickedLat: selectedLocation.lat,
+      pickedLng: selectedLocation.lng,
+    });
+  }, [navigation, selectedLocation]);
+
+  useLayoutEffect(() => {
+    navigation.setOptions({
+      headerRight: ({ tintColor }) => (
+        <IconButton
+          icon="save"
+          color={tintColor ?? "black"}
+          size={24}
+          onPress={savePickedLocationHandler}
+        />
+      ),
+    });
+  }, [navigation, savePickedLocationHandler]);
+
+  return (
+    <MapView
+      style={styles.map}
+      initialRegion={INITIAL_REGION}
+      onPress={selectLocationHandler}
+    >
+      {selectedLocation && (
+        <Marker
+          title="Picked Location"
+          coordinate={{
+            latitude: selectedLocation.lat,
+            longitude: selectedLocation.lng,
+          }}
+        />
+      )}
+    </MapView>
+  );
+};
+
+export default Map;
+
+const styles = StyleSheet.create({
+  map: {
+    flex: 1,
+  },
+});
