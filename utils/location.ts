74f1162bb4e2b2c262189b0297a99d4cb1d479e2
Ihsanullah-Yasar <ihsanullahyasar70@gmail.com>
2026-03-07 00:43:27 +0430

    helper to construct map preview url(ts new verions)

---
+++ utils/location.ts
@@ -0,0 +1,11 @@
+// const GOOGLE_API_KEY = process.env.EXPO_PUBLIC_GOOGLE_MAPS_API_KEY;
+const GOOGLE_API_KEY = "AIzaSyDmefiGMKVaUqxtRtE2LHI2AwUePHK4G6Q";
+
+
+export function getMapImagePreviewUrl(lat: number, lng: number): string {
+  const imagePreviewUrl = `https://maps.googleapis.com/maps/api/staticmap?center=${lat},${lng}&zoom=14&size=400x200&maptype=roadmap
+&markers=color:red%7Clabel:S%7C${lat},${lng}&key=${GOOGLE_API_KEY}`;
+console.log("image preview url",imagePreviewUrl);
+  return imagePreviewUrl;
+}
+  
