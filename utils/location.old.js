19e563161cec7aaa9d4c11dddcc6bb48a3850537
Ihsanullah-Yasar <ihsanullahyasar70@gmail.com>
2026-03-07 00:43:02 +0430

    helper to construct map preview url(js old verions)

---
+++ utils/location.old.js
@@ -0,0 +1,10 @@
+const GOOGLE_API_KEY = "AIzaSyDjw0wxpQ1_CWKm01-nfYMKs1I-Gtvtgy0";
+
+export function getMapImagePreviewUrl(lat, lng) {
+  const imagePreviewUrl = `https://maps.googleapis.com/maps/api/staticmap?center=${lat},${lng}&zoom=13&size=400x200&maptype=roadmap
+&markers=color:red%7Clabel:S%7C${lat},${lng}&key=${GOOGLE_API_KEY}`;
+  return imagePreviewUrl;
+}
+
+//`https://maps.googleapis.com/maps/api/staticmap?center=${lat},${lng}&zoom=13&size=400x200&maptype=roadmap
+//&markers=color:red%7Clabel:S%7C${lat},${lng}&key=AIzaSyDjw0wxpQ1_CWKm01-nfYMKs1I-Gtvtgy0&signature=YOUR_SIGNATURE`;
