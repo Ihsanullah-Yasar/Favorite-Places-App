ef7dd3f9ce9a8420313dec0540ee585544c43dfe
Ihsanullah-Yasar <ihsanullahyasar70@gmail.com>
2026-03-07 12:36:12 +0430

    refactor: secret api key moved to .env and used

--- utils/location.ts
+++ utils/location.ts
@@ -1,5 +1,5 @@
 // const GOOGLE_API_KEY = process.env.EXPO_PUBLIC_GOOGLE_MAPS_API_KEY;
-const GOOGLE_API_KEY = "AIzaSyDmefiGMKVaUqxtRtE2LHI2AwUePHK4G6Q";
+const GOOGLE_API_KEY = process.env.EXPO_PUBLIC_GOOGLE_MAPS_API_KEY;
 
 
 export function getMapImagePreviewUrl(lat: number, lng: number): string {
