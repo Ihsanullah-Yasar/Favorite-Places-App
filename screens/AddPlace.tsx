e863f29559f5a767d3610d1485f3d89c5b293e78
Ihsanullah-Yasar <ihsanullahyasar70@gmail.com>
2026-03-15 14:53:04 +0430

    feat: place saving feature added

--- screens/AddPlace.tsx
+++ screens/AddPlace.tsx
@@ -2,12 +2,17 @@ import { NativeStackScreenProps } from "@react-navigation/native-stack";
 import PlaceForm from "../components/Places/PlaceForm";
 import React, { JSX } from "react";
 import { RootStackParamList } from "../types/navigation";
+import { ILocation } from "../types/place";
+import { Place } from "../models/Place";
 
 type AddPlaceProps = NativeStackScreenProps<RootStackParamList, "AddPlace">;
 
-const AddPlace: React.FC<AddPlaceProps> = ({ navigation }): JSX.Element => {
-  return <PlaceForm />;
-};
+function AddPlace({ navigation }: AddPlaceProps) {
+  function createPlaceHandler(place: Place) {
+    navigation.navigate("AllPlaces", { place });
+  }
+  return <PlaceForm onCreatePlace={createPlaceHandler} />;
+}
 
 // function AddPlace() {
 //   return <PlaceForm />;
