a0ae67b3c06957130601d90d350cff63818e73f0
Ihsanullah-Yasar <ihsanullahyasar70@gmail.com>
2026-03-21 12:28:47 +0100

    refactor: prop added

--- components/Places/PlacesList.tsx
+++ components/Places/PlacesList.tsx
@@ -9,6 +9,7 @@ interface PlaceListProps {
   onPlacePress?: (placeId: string) => void;
   // keyExtractor?: (item: Place, index: number) => string;
   emptyMessage?: string;
+  testID?: string;
 }
 
 //Default empty state component
@@ -24,6 +25,7 @@ const PlaceList: React.FC<PlaceListProps> = ({
   places,
   onPlacePress,
   emptyMessage = "No places to display.",
+  testID = "place-list",
 }) => {
   // Memoized key extractor
   const keyExtractor = useCallback((item: Place): string => {
